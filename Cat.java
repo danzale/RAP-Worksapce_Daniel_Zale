bb393f13e0b01c6034c4fb397672fcd23eedc310
Daniel Zale <46909245+danzale@users.noreply.github.com>
2021-04-10 22:18:11 -0400

    Cat.java
    This is the ASCII String format homework

---
+++ Cat.java
@@ -0,0 +1,21 @@
+
+public class Cat {
+	public static void main(String args[]) {
+		System.out.print("\n  |\\__/,|   (`\\\r\n"
+				+ "  |_ _  |.--.) )\r\n"
+				+ "  ( T   )     /\r\n"
+				+ " (((^_(((/(((_>");
+		
+		System.out.println();
+		System.out.print(String.format("\n%1$s\n%2$s%3$1.1f%4$s\n%5$s\n%6$s\n%7$s%8$b%9$s", 
+										" |\\__/,|   (`\\", 
+										" |",
+										0.0,
+										"  |.--.) )", 
+										" ( T   )     /", 
+										"(((^_(((/(((_>",
+										"It's ",
+										true,
+										" this is feilx"));
+	}
+}
